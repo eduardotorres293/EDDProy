3e7e88c05c58fef928d16916e2deaf87e6167816
pc <al23760375@ite.edu.mx>
2024-11-25 17:14:39 -0800

    - Añadidos metodos de intercalacion y burbuja

---
+++ EDDProy/Metodos de ordenamiento/Intercalacion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDDemo.Metodos_de_ordenamiento
+{
+    public class Intercalacion
+    {
+        public List<int> IntercalarListas(List<int> lista1, List<int> lista2)
+        {
+            List<int> listaIntercalada = new List<int>();
+            int i = 0, j = 0;
+            while (i < lista1.Count && j < lista2.Count)
+            {
+                if (lista1[i] < lista2[j])
+                {
+                    listaIntercalada.Add(lista1[i]);
+                    i++;
+                }
+                else
+                {
+                    listaIntercalada.Add(lista2[j]);
+                    j++;
+                }
+            }
+            while (i < lista1.Count)
+            {
+                listaIntercalada.Add(lista1[i]);
+                i++;
+            }
+            while (j < lista2.Count)
+            {
+                listaIntercalada.Add(lista2[j]);
+                j++;
+            }
+
+            return listaIntercalada;
+        }
+    }
+}
